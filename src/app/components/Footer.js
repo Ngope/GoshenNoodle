7fca5497575eb94c3df966f40c85e3110a0238dc
Peter Ngo <peterngo2496@gmail.com>
2024-12-25 19:02:31 -0600

    light fixes to css and files

---
+++ src/app/components/Footer.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import '../styles/styles.css'; // Importing CSS styles
+
+export default function Footer() {
+  return (
+    <footer className="footer">
+    <div className="container">
+      <p>&copy; 2024 Goshen Noodles. All rights reserved.</p>
+    </div>
+  </footer>
+  );
+}
