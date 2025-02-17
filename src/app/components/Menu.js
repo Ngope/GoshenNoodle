9c71992a97f565cfd69d291882bc36d6113af3b0
Peterson Ngo <ngop0629@gmail.com>
2025-02-16 19:07:52 -0600

    fix prices

--- src/app/components/Menu.js
+++ src/app/components/Menu.js
@@ -13,37 +13,37 @@ export default function Menu() {
                   <p>10. Eggrolls (2 pcs)</p>
                   <p className="options"><i>  Pork, Veggie</i></p>
                 </div>
-                <p className="price">$1.95</p>
+                <p className="price">$1.99</p>
               </li>
               <li className='menuItem'>
                 <div className="foodItems">
                 <p>11. Spring Rolls (2 pcs)</p>
                 <p className="options"><i>  Pork, Shrimp, Pork + Shrimp</i></p>
                 </div>
-                <p className="price">$3.45</p>
+                <p className="price">$4.99</p>
               </li>
               <li className='menuItem'>
                 <p>12. Crispy Shrimp Wraps (5 pcs)</p>
-                <p className="price">$3.95</p>
+                <p className="price">$4.99</p>
               </li>
               <li className='menuItem'>
                 <p>13. Crab Rangoons (3 pcs)</p>
-                <p className="price">$2.50</p>
+                <p className="price">$3.99</p>
               </li>
               <li className='menuItem'>
                 <p>14. Shrimp Eggrolls (2 pcs)</p>
-                <p className="price">$2.50</p>
+                <p className="price">$2.99</p>
               </li>
               <li className='menuItem'>
                 <div className="foodItems">
                   <p>15. Dumplings (5 pcs)</p>
                   <p className="options"><i>  Pork, Veggie, Chicken</i></p>
                 </div>
-                <p className="price">$4.95</p>
+                <p className="price">$5.99</p>
               </li>
               <li className='menuItem'>
                 <p>16. Chicken Wings (5 pcs)</p>
-                <p className="price">$5.95</p>
+                <p className="price">$6.99</p>
               </li>
             </ul>
             <p><i>Reminder that there are mulitple choices for that menu items. E.X Pork or Veggie Eggrolls</i></p>
@@ -53,30 +53,34 @@ export default function Menu() {
             <ul>
               <li className='menuItem'>
                 <p>20. Shrimp Fried Rice</p>
-                <p className="price">$7.95</p>
+                <p className="price">$11.99</p>
               </li>
               <li className='menuItem'>
                 <p>21. Beef Fried Rice</p>
-                <p className="price">$7.95</p>
+                <p className="price">$11.99</p>
               </li>
               <li className='menuItem'>
                 <p>22. Chicken Fried Rice</p>
-                <p className="price">$6.95</p>
+                <p className="price">$11.99</p>
               </li>
               <li className='menuItem'>
                 <p>23. Veggie Fried Rice</p>
-                <p className="price">$6.50</p>
+                <p className="price">$11.99</p>
               </li>
               <li className='menuItem'>
                 <p>24. Steam Rice with Pork Chop</p>
-                <p className="price">$9.95</p>
+                <p className="price">$13.99</p>
               </li>
               <li className='menuItem'>
                 <p>25. Steam Rice with Beef Steak Chunks</p>
-                <p className="price">$9.95</p>
+                <p className="price">$14.99</p>
+              </li>
+              <li className='menuItem'>
+                <p>26. BBQ Fried Rice</p>
+                <p className="price">$11.99</p>
               </li>
             </ul>
-            <p><i>Make it a combo for $2.50 (2 Eggrolls + Choice of Beef, Veggie, or Chicken Broth)</i></p>
+            <p><i>Make it a combo for $2.99 (2 Eggrolls + Choice of Beef, Veggie, or Chicken Broth)</i></p>
           </div>
           <div className="menuCategory">
             <h3>Pho</h3>
@@ -87,8 +91,8 @@ export default function Menu() {
                   <p className="options"><i>  Pho Dac Biet: (Meatballs, Round Steak, Brisket)</i></p>
                 </div>
                 <div className="priceItems">
-                  <p className="price">L: $11.95</p>
-                  <p className="price">S: $10.95</p>
+                  <p className="price">L: $13.99</p>
+                  <p className="price">S: $12.99</p>
                 </div>
               </li>
               <li className='menuItem'>
@@ -97,8 +101,8 @@ export default function Menu() {
                   <p className="options"><i>  Pho Tai Nam</i></p>
                 </div>
                 <div className="priceItems">
-                  <p className="price">L: $10.50</p>
-                  <p className="price">S: $9.50</p>
+                  <p className="price">L: $12.99</p>
+                  <p className="price">S: $11.99</p>
                 </div>
               </li>
               <li className='menuItem'>
@@ -107,8 +111,8 @@ export default function Menu() {
                   <p className="options"><i>  Pho Tai</i></p>
                 </div>
                 <div className="priceItems">
-                  <p className="price">L: $10.50</p>
-                  <p className="price">S: $9.50</p>
+                  <p className="price">L: $12.99</p>
+                  <p className="price">S: $11.99</p>
                 </div>
               </li>
               <li className='menuItem'>
@@ -117,8 +121,8 @@ export default function Menu() {
                   <p className="options"><i>  Pho Bo Vien</i></p>
                 </div>
                 <div className="priceItems">
-                  <p className="price">L: $10.50</p>
-                  <p className="price">S: $9.50</p>
+                  <p className="price">L: $12.99</p>
+                  <p className="price">S: $11.99</p>
                 </div>
               </li>
               <li className='menuItem'>
@@ -127,15 +131,38 @@ export default function Menu() {
                   <p className="options"><i>  Pho Ga</i></p>
                 </div>
                 <div className="priceItems">
-                  <p className="price">L: $10.00</p>
-                  <p className="price">S: $9.00</p>
+                  <p className="price">L: $12.99</p>
+                  <p className="price">S: $11.99</p>
                 </div>
               </li>
               <li className='menuItem'>
-                <p>35. Veggie Pho</p>
+                <div className="foodItems">
+                  <p>35. Veggie Pho</p>
+                  <p className="options"><i>  Pho Chay</i></p>
+                </div>
                 <div className="priceItems">
-                  <p className="price">L: $10.50</p>
-                  <p className="price">S: $9.50</p>
+                  <p className="price">L: $12.99</p>
+                  <p className="price">S: $11.99</p>
+                </div>
+              </li>
+              <li className='menuItem'>
+                <div className="foodItems">
+                  <p>36. Shrimp Pho</p>
+                  <p className="options"><i>  Pho Tom</i></p>
+                </div>
+                <div className="priceItems">
+                  <p className="price">L: $12.99</p>
+                  <p className="price">S: $11.99</p>
+                </div>
+              </li>
+              <li className='menuItem'>
+                <div className="foodItems">
+                 <p>37. Seafood Pho</p>
+                  <p className="options"><i>  Pho Hai San</i></p>
+                </div>
+                <div className="priceItems">
+                  <p className="price">L: $13.99</p>
+                  <p className="price">S: $11.99</p>
                 </div>
               </li>
             </ul>
@@ -146,27 +173,31 @@ export default function Menu() {
             <ul>
               <li className='menuItem'>
                 <p>60. Combination Banh Mi (Chk + Pork + Beef)</p>
-                <p className="price">$6.00</p>
+                <p className="price">$7.49</p>
               </li>
               <li className='menuItem'>
                 <p>61. Grilled Chicken Banh Mi</p>
-                <p className="price">$5.00</p>
+                <p className="price">$6.99</p>
               </li>
               <li className='menuItem'>
                 <p>62. Grilled Beef Banh Mi</p>
-                <p className="price">$5.50</p>
+                <p className="price">$7.49</p>
               </li>
               <li className='menuItem'>
-                <p>63. BBQ Pork Banh Mi</p>
-                <p className="price">$5.50</p>
+                <p>64. BBQ Pork Banh Mi</p>
+                <p className="price">$6.99</p>
               </li>
               <li className='menuItem'>
-                <p>64. Pork Terrine Banh Mi</p>
-                <p className="price">$5.50</p>
+                <p>65. Pork Terrine Banh Mi</p>
+                <p className="price">$7.49</p>
               </li>
               <li className='menuItem'>
-                <p>65. Tofu Banh Mi</p>
-                <p className="price">$5.50</p>
+                <p>66. Tofu Banh Mi</p>
+                <p className="price">$6.99</p>
+              </li>
+              <li className='menuItem'>
+                <p>67. Spicy Korean BBQ Banh Mi</p>
+                <p className="price">$6.99</p>
               </li>
             </ul>
             <p><i>Served with Pickled Daikon/Carrots, Cucumber, Jalepenos, Cilantro, and Mayo. Can Change.</i></p>
@@ -176,15 +207,39 @@ export default function Menu() {
             <ul>
               <li className='menuItem'>
                 <p>50. Grilled Beef & Eggroll Vermicelli</p>
-                <p className="price">$8.50</p>
+                <p className="price">$12.99</p>
               </li>
               <li className='menuItem'>
                 <p>51. Grilled Chicken & Eggroll Vermicelli</p>
-                <p className="price">$5.00</p>
+                <p className="price">$12.99</p>
               </li>
               <li className='menuItem'>
                 <p>52. Eggroll Vermicelli</p>
-                <p className="price">$5.50</p>
+                <p className="price">$12.99</p>
+              </li>
+              <li className='menuItem'>
+                <p>53. Grilled Shrimp Vermicelli</p>
+                <p className="price">$12.99</p>
+              </li>
+              <li className='menuItem'>
+                <p>54. Grilled Meatball & Eggroll Vermicelli</p>
+                <p className="price">$13.50</p>
+              </li>
+              <li className='menuItem'>
+                <p>55. Grilled Chicken & Eggroll Vermicelli</p>
+                <p className="price">$13.50</p>
+              </li>
+              <li className='menuItem'>
+                <p>56. Grilled Beef & Shrimp Vermicelli</p>
+                <p className="price">$13.50</p>
+              </li>
+              <li className='menuItem'>
+                <p>57. Grilled Chicken & Shrimp Vermicelli</p>
+                <p className="price">$13.99</p>
+              </li>
+              <li className='menuItem'>
+                <p>58. Grilled Pork & Shrimp Vermicelli</p>
+                <p className="price">$13.99</p>
               </li>
             </ul>
             <p><i>Served with a side of Sweet Chili Fish Sauce. All Servings come with Lettuce, Cucumber, and Peanuts</i></p>
@@ -195,9 +250,9 @@ export default function Menu() {
               <li className='menuItem'>
                 <div className="foodItems">
                   <p>43. Stir Fry Egg Noodles</p>
-                  <p className="options"><i>  Pork, Chicken, or Beef</i></p>
+                  <p className="options"><i>  Pork, Chicken, Beef, or Veggies</i></p>
                 </div>
-                <p className="price">$8.50</p>
+                <p className="price">$12.99</p>
               </li>
             </ul>
           </div>
