5a6ef14ffb5a3925052348d441507a698ebb4e31
Peter Ngo <peterngo2496@gmail.com>
2025-02-20 13:09:05 -0600

    made nav bar mobile friendly, collapses when in mobile

--- src/app/page.js
+++ src/app/page.js
@@ -1,6 +1,6 @@
 
 import styles from './styles/styles.css'; // Importing CSS styles
-import Jumbotron from './components/jumbotron';
+import Jumbotron from './components/Jumbotron';
 import Navbar from './components/Navbar';
 import Landing from './components/Landing';
 import Menu from './components/Menu';
@@ -34,7 +34,7 @@ export default function Home() {
             </div>
           </div>
           <div className="google-map-container">
-            <iframe class="google-map" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d962.588370033123!2d-85.81656569699484!3d41.56700305677343!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x8816ed60bd63323d%3A0x1c1eb390883272c2!2sGoshen%20Noodles!5e0!3m2!1sen!2sus!4v1715649856224!5m2!1sen!2sus" width="400" height="300" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+            <iframe className="google-map" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d962.588370033123!2d-85.81656569699484!3d41.56700305677343!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x8816ed60bd63323d%3A0x1c1eb390883272c2!2sGoshen%20Noodles!5e0!3m2!1sen!2sus!4v1715649856224!5m2!1sen!2sus" width="400" height="300" allowFullScreen="" loading="lazy" referrerPolicy="no-referrer-when-downgrade"></iframe>
           </div>
         </div> {/* ends info section */}
        <Menu />
